d7d712f822f49bbf5cc6eaaba831eedf26bf34c7
Holger Trahe <trahe@mac.com>
2023-09-01 16:34:10 +0200

    release 0.2.1

--- README.md
+++ README.md
@@ -1,86 +1,46 @@
-<div align="center">
+# GLDF-RS-WASM
 
-  <h1><code>wasm-pack-template</code></h1>
+#### The wasm version of [gldr-rs](https://crates.io/gldr-rs)
 
-  <strong>A template for kick starting a Rust and WebAssembly project using <a href="https://github.com/rustwasm/wasm-pack">wasm-pack</a>.</strong>
+GLDF-RS-WASM is a WebAssembly (Wasm) version of the Global Lighting Data Format (GLDF) library. It allows you to work with GLDF data directly in the browser by leveraging WebAssembly technology. GLDF is a standardized format for describing lighting products and their technical details.
+### Release Notes
+- 0.2.1 usage of new gldf-rs 0.2.1
+- Inheritance and overwriting of properties (needed bcs of reqwest)
 
-  <p>
-    <a href="https://travis-ci.org/rustwasm/wasm-pack-template"><img src="https://img.shields.io/travis/rustwasm/wasm-pack-template.svg?style=flat-square" alt="Build Status" /></a>
-  </p>
 
-  <h3>
-    <a href="https://rustwasm.github.io/docs/wasm-pack/tutorials/npm-browser-packages/index.html">Tutorial</a>
-    <span> | </span>
-    <a href="https://discordapp.com/channels/442252698964721669/443151097398296587">Chat</a>
-  </h3>
+## Features
 
-  <sub>Built with 🦀🕸 by <a href="https://rustwasm.github.io/">The Rust and WebAssembly Working Group</a></sub>
-</div>
+- Deserialize GLDF files on the client-side using WebAssembly.
+- Interact with GLDF data directly within web applications.
+- Use GLDF data for dynamic visualization and analysis in the browser.
 
-## About
+## Usage
 
-[**📚 Read this template tutorial! 📚**][template-docs]
+1. Include the GLDF-RS-WASM JavaScript module in your HTML:
 
-This template is designed for compiling Rust libraries into WebAssembly and
-publishing the resulting package to NPM.
+```html
+<!DOCTYPE html><html lang="en"><head>
+    <meta charset="utf-8">
+    <title>GLDF • File Upload</title>
 
-Be sure to check out [other `wasm-pack` tutorials online][tutorials] for other
-templates and usages of `wasm-pack`.
+    <script type="module">import init from '/gldf-rs-wasm-e633e9b682fa57a5.js';init('/gldf-rs-wasm-e633e9b682fa57a5_bg.wasm');</script>
+    <link rel="stylesheet" href="/styles-a8c36e60fd065d7c.css">
+    <link rel="stylesheet" href="/font-awesome.min.css">
+  
+<link rel="preload" href="/gldf-rs-wasm-e633e9b682fa57a5_bg.wasm" as="fetch" type="application/wasm" crossorigin="">
+<link rel="modulepreload" href="/gldf-rs-wasm-e633e9b682fa57a5.js"></head>
 
-[tutorials]: https://rustwasm.github.io/docs/wasm-pack/tutorials/index.html
-[template-docs]: https://rustwasm.github.io/docs/wasm-pack/tutorials/npm-browser-packages/index.html
+  <body>
 
-## 🚴 Usage
-
-### 🐑 Use `cargo generate` to Clone this Template
-
-[Learn more about `cargo generate` here.](https://github.com/ashleygwilliams/cargo-generate)
-
-```
-cargo generate --git https://github.com/rustwasm/wasm-pack-template.git --name my-project
-cd my-project
+</body></html>
 ```
 
-### 🛠️ Build with `wasm-pack build`
+That's it! You can now use the `gldf-rs-wasm` module in your JavaScript code.
+The working example can be found here:   
 
-```
-wasm-pack build
-```
+### https://gldf.icu
 
-### 🔬 Test in Headless Browsers with `wasm-pack test`
+There you have the advantage, that included eulumdat files, can be directly opened into  
+### https://eulumdat.icu,  
 
-```
-wasm-pack test --headless --firefox
-```
-
-### 🎁 Publish to NPM with `wasm-pack publish`
-
-```
-wasm-pack publish
-```
-
-## 🔋 Batteries Included
-
-* [`wasm-bindgen`](https://github.com/rustwasm/wasm-bindgen) for communicating
-  between WebAssembly and JavaScript.
-* [`console_error_panic_hook`](https://github.com/rustwasm/console_error_panic_hook)
-  for logging panic messages to the developer console.
-* [`wee_alloc`](https://github.com/rustwasm/wee_alloc), an allocator optimized
-  for small code size.
-* `LICENSE-APACHE` and `LICENSE-MIT`: most Rust projects are licensed this way, so these are included for you
-
-## License
-
-Licensed under either of
-
-* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
-* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
-
-at your option.
-
-### Contribution
-
-Unless you explicitly state otherwise, any contribution intentionally
-submitted for inclusion in the work by you, as defined in the Apache-2.0
-license, shall be dual licensed as above, without any additional terms or
-conditions.
+which is an WASM Eulumdat Editor and Viewer, which can as well export to IESNA Format.
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use std::error::Error as StdError;
 
 use base64::encode;
 use gloo::file::callbacks::FileReader;
-use gloo::file::File;
+use gloo::file::{ObjectUrl,File};
 use gloo::console;
-use js_sys::Date;
-use web_sys::{DragEvent, Event, FileList, HtmlInputElement};
+use js_sys::{Date, JsString};
+use web_sys::{DragEvent, Event, FileList, HtmlInputElement, Blob};
 use yew::html::TargetCast;
 use yew::{html, Callback, Component, Context, Html};
 use yew::prelude::*;
@@ -18,9 +18,40 @@ use yew::prelude::*;
 // #use yew::ServerRenderer;
 use gldf_rs::gldf::GldfProduct;
 use gldf_rs::{FileBufGldf, BufFile};
+use reqwest_wasm;
+// use wasm_bindgen_test::console_log;
 use crate::console::console;
+// mod bindings;
+// use bindings::string_test;
 
+pub async  fn fetch_text_from_url(url: &str) -> Result<String, reqwest_wasm::Error> {
+    let response = reqwest_wasm::get(url).await?;
+    response.text().await
+}
 
+struct WasmGldfProduct(GldfProduct);
+impl WasmGldfProduct{
+    pub async fn get_ldc_by_id(self: &Self, file_id: String) -> anyhow::Result<String> {
+        let mut result: String = "".to_owned();
+        for f in self.0.general_definitions.files.file.iter() {
+            if f.id == file_id{
+                let mut ldc_path = "ldc/".to_owned();
+                let file_name = f.file_name.to_owned();
+                if f.type_attr == "url" {
+                    result.push_str(fetch_text_from_url(&file_name).await?.as_str());
+                } else {
+                    ldc_path.push_str(&file_name);
+                    result.push_str(&self.0.load_gldf_file_str(ldc_path)?.as_str());
+                }
+            }
+        }
+        Ok(result)
+    }
+    pub fn load_gldf_from_buf_all(buf: Vec<u8>) -> anyhow::Result<FileBufGldf> {
+        let file_buf = GldfProduct::load_gldf_from_buf_all(buf)?;
+        Ok(file_buf)
+    }
+}
 struct FileDetails {
     name: String,
     file_type: String,
@@ -37,6 +68,7 @@ pub struct App {
     files: Vec<FileDetails>,
 }
 
+#[recursion_limit = "256"]
 impl Component for App {
     type Message = Msg;
     type Properties = ();
@@ -64,6 +96,7 @@ impl Component for App {
                 for file in files.into_iter() {
                     let file_name = file.name();
                     let file_type = file.raw_mime_type();
+                    console::log!("A File:", file_name.as_str(), file_type.as_str());
 
                     let task = {
                         let link = ctx.link().clone();
@@ -84,11 +117,23 @@ impl Component for App {
             }
         }
     }
-
+    #[recursion_limit = "256"]
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <div id="wrapper">
-                <p id="title">{ "Upload GLDF Datei" }</p>
+                <p id="title">{ "Upload GLDF File" }</p>
+                <p id="info">{ "Copyright Holger Trahe" }</p>
+                <p id="info"><a href="mailto:trahe@mac.com">{"trahe@mac.com"}</a></p>
+                <p id="info"><a id="title" href="https://gldf.io">{"Click here for GLDF Information"}</a></p>
+                <p id="info"><a id="title" href="https://eulumdat.icu/">{"Also check QLumEdit in the Browser"}</a></p>
+                <p>{"This is without any backend, no data is uploaded"}</p>
+                <p>{"Everything happens only in your browser"}</p>
+                <p>{"Actually it is all in a signed .wasm, to be even more trustfull!"}</p>
+                <p>{"Ask your tech-guys they will confirm ;)"}</p>
+                <p>{"It shows the content of the GLDF Container"}</p>
+                <p>{"And for more the convenient workflow, it shows as well the JSON version"}</p>
+                <p>{"New Feature now has the Open in QLumEdit, which will load the .ldt file in QLumEdit in a new tab"}</p>
+
                 <label for="file-upload">
                     <div
                         id="drop-container"
@@ -105,7 +150,7 @@ impl Component for App {
                         })}
                     >
                         <i class="fa fa-cloud-upload"></i>
-                        <p>{"Drop your images here or click to select"}</p>
+                        <p>{"Drop your GLDF file here or click to select"}</p>
                     </div>
                 </label>
                 <input
@@ -127,15 +172,28 @@ impl Component for App {
 }
 
 fn parse_file_for_gldf(file: &FileDetails) -> FileBufGldf{
-    return GldfProduct::load_gldf_from_buf_all(file.data.clone()).unwrap();
+    return WasmGldfProduct::load_gldf_from_buf_all(file.data.clone()).unwrap();
+}
+
+pub fn get_blob(buf_file: &BufFile) -> String {
+    let uint8arr = js_sys::Uint8Array::new(&unsafe { js_sys::Uint8Array::view(&buf_file.content.clone().unwrap()) }.into());
+    let array = js_sys::Array::new();
+    array.push(&uint8arr.buffer());
+    let blob = Blob::new_with_str_sequence_and_options(
+        &array,
+        web_sys::BlobPropertyBag::new().type_("application/vnd.openxmlformats-officedocument.wordprocessingml.document"),
+    ).unwrap();
+    let download_url = web_sys::Url::create_object_url_with_blob(&blob).unwrap();
+    return download_url
 }
 
 impl App {
 
     fn view_file(file: &FileDetails) -> Html {
         fn view_gldf_file(buf_file: &BufFile) -> Html {
+            // console_log!("Action for file_type{?}:", buf_file);
             html! {
-                <div id="buf_file">{ format!("{}", buf_file.name.clone().unwrap_or("".to_string())) }
+                <div id="buf_file"><p>{ format!("{}", buf_file.name.clone().unwrap_or("".to_string())) }</p>
                     if buf_file.name.clone().expect("REASON").to_lowercase().ends_with(".jpg"){
                         <img src={format!("data:image/jpeg;base64,{}", encode(buf_file.clone().content.unwrap_or(Vec::new()))  )} />
                     }
@@ -143,7 +201,13 @@ impl App {
                         <img src={format!("data:image/png;base64,{}", encode(buf_file.clone().content.unwrap_or(Vec::new()))  )} />
                     }
                     else if buf_file.name.clone().expect("REASON").to_lowercase().ends_with(".ldt"){
-                        <textarea value={format!(r"{}", String::from_utf8_lossy(buf_file.content.clone().unwrap().as_slice()))}></textarea>
+                        // <a href={format!(r"http://gldf.icu/QLumEdit/QLumEdit.html?ldc_name={}&ldc_base64={}",buf_file.name, base64::encode(buf_file.content.clone()))}</a>
+                        // base64 is the bad approach, as it transfers data to the server, thus can be somehow intercepted and is just unneeded garbage as the server shall not know anyhow
+                        // <a target="_blank" href={format!(r"https://eulumdat.icu/?ldc_name=trahe.ldt&ldc_base64={}", encode(buf_file.clone().content.unwrap_or(Vec::new())))}>{"Open in QLumEdit"}</a>
+                        <a href={format!(r"/QLumEdit/QLumEdit.html?ldc_name=trahe.ldt&ldc_blob_url={}", get_blob(buf_file))}>{"Open in QLumEdit"}</a>
+                        // <a target="_blank" href={format!(r"/QLumEdit/QLumEdit.html?ldc_name=trahe.ldt&ldc_blob_url={}", get_blob(buf_file))}>{"Open in QLumEdit"}</a>
+                        <br/><textarea value={format!(r"{}", String::from_utf8_lossy(buf_file.content.clone().unwrap().as_slice()))}></textarea>
+                   //     <textarea value={format!(r"{}", string_test("test"))}></textarea>
                     }
                     else if buf_file.name.clone().expect("REASON").to_lowercase().ends_with(".xml"){
                         <textarea value={format!(r"{}", String::from_utf8_lossy(buf_file.content.clone().unwrap().as_slice()))}></textarea>
@@ -156,14 +220,19 @@ impl App {
         let mut have_gldf = false;
         let loaded = parse_file_for_gldf(file);
         let buf_files = loaded.files.to_vec();
+        console::log!("Files:{?}", buf_files.len());
         console::log!("Author:", loaded.gldf.header.author.as_str());
         html! {
             <div class="preview-tile">
                 <p class="preview-name">{ format!("{}", file.name) }</p>
                 <div class="preview-media">
-                    if file.file_type.contains("image") {
+                    if file.file_type.contains("url") {
+                        <img src={ format!("{}", file.name) } />
+                    }
+                    else if file.file_type.contains("image") {
                         <img src={format!("data:{};base64,{}", file.file_type, encode(&file.data))} />
-                    } else if file.file_type.contains("video") {
+                    }
+                    else if file.file_type.contains("video") {
                         <video controls={true}>
                             <source src={format!("data:{};base64,{}", file.file_type, encode(&file.data))} type={file.file_type.clone()}/>
                         </video>
@@ -172,6 +241,13 @@ impl App {
                         <textarea value={format!(r#"{{"product": {}}}"#, loaded.gldf.to_pretty_json().expect("REASON").as_str())}></textarea>
                         { for buf_files.iter().map(view_gldf_file) }
                     }
+                    else {
+                        <p>{ "Unknown file type" }</p>
+                    }
+                    // for file in loaded.gldf.general_definitions.files.file.iter() {
+                    //     let content_type = &file.content_type;
+                    //     <source src={format!("{}", file.file_type)}/>
+                    // }
 
                 </div>
             </div>
